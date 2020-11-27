f77a89f964b72e944af4dad31edf2f3fb0459923
drspz <spz0707k@gmail.com>
2020-11-27 10:19:26 +0900

    空のメッセージが送信できないバリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: "rooms#index"
+  root to: "rooms#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "messages#index"
+  # root to: "messages#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create] do
     resources :messages, only: [:index, :create]
+  end
 end
